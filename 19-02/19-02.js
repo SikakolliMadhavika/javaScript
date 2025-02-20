2893aed6a0b2fa5bda2a2604b01aee2bad9bf023
SikakolliMadhavika-github <140150457+SikakolliMadhavika@users.noreply.github.com>
2025-02-20 16:17:38 +0530

    Add files via upload

--- 19-02/19-02.js
+++ 19-02/19-02.js
@@ -1,4 +1,5 @@
 // Question - 1
+// using anonymous
 function processNumbers( num1, num2, callbackfun){
     return callbackfun(num1 , num2)
 }
@@ -7,8 +8,17 @@ var result = processNumbers(3, 4, function(x,y){
 })
 console.log("Sum is",result)
 
+// using arrow
+function processNumbers(num1, num2, arrowfun){
+    return arrowfun(num1, num2)
+}
+var res = processNumbers(3,4,(x,y) => x+y)
+console.log("Sum:",res)
+
+
 
 // Question - 2
+// using anonymous
 function greet(callbackfun){
     return callbackfun("Alice")
 }
@@ -17,8 +27,16 @@ var result = greet(function(name){
 })
 console.log(result)
 
+// using arrow
+function greet(arrowfun){
+    return arrowfun("Alice")
+}
+var res = greet(name => "Hello, ["+name+"]!")
+console.log(res)
+
 
 // Question - 3
+// using anonymous
 function calculate(num1, num2, callbackfun){
     return callbackfun(num1,num2)
 }
@@ -26,3 +44,11 @@ var res=calculate(10,5,function(x,y){
     return "Difference is "+ (x-y)
 })
 console.log(res)
+
+
+// using arrow
+function calculate(num1, num2,arrowfun){
+    return arrowfun(num1, num2)
+}
+var res = calculate(10,5,(x,y) => x-y)
+console.log("Difference:",res)
