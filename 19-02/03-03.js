d4f333dc480c89bb98cc9679c0a88156533e6330
SikakolliMadhavika-github <140150457+SikakolliMadhavika@users.noreply.github.com>
2025-03-03 21:46:54 +0530

    Add files via upload

---
+++ 19-02/03-03.js
@@ -0,0 +1,53 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// console.log("Try programiz.pro");
+
+// 1. loop to print numbers
+for(var i=1; i<=10;i++){
+    console.log(i)
+}
+
+
+// 2. loop to find sum
+var sum=0
+for(var i=1; i<=100;i++){
+    sum+=i
+}
+console.log(sum)
+
+
+// 3. for loop to print table
+for(var i=1; i<=10;i++){
+    console.log(`7 * ${i} = ${i*7}`)
+}
+
+
+// 4. for loop to print even
+for(var i=2;i<=20;i=i+2){
+    console.log(i)
+}
+
+// 5. for loop to find fact
+var num=5
+var mul=1
+for (var i=2;i<=num;i++){
+    mul*=i
+}
+console.log(mul)
+
+
+// 6. for loop to find power
+var base=2
+var power=5
+var res=1
+for (var i=1; i<=power;i++){
+    res*=base
+}
+console.log(res)
+
+
+// 7. print odd in reverse
+for(var i=20; i>=1;i--){
+    console.log(i)
+}
